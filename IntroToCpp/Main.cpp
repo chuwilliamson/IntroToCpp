8508ba373fbc41746bbd94a605d44a9fbceb56f6
Matthew Williamson <matthew.williamson@aie.edu.au>
2018-08-13 16:23:19 -0500

    Add project files.

---
+++ IntroToCpp/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//int - return type//
+//main - name of function//
+//() - denotes a function declaration
+int main()
+{
+	//std:: - denotes a namespace
+	//cout - output stream
+	//<< - stream operator
+	//"Hello World!" - string to print
+	//<< - stream operator
+	printf("Hello World! \n");
+	
+return 0;
+}
